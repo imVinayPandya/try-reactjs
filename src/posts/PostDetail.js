c662876684e2eb972585fb8ae76578c29805b16d
Vinay Pandya <005vinay@gmail.com>
2018-07-17 15:03:06 +0530

    Callbacks using props

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
 class PostDetail extends Component {
-	// constructor(props){
-	//   super(props);
-	//   this.titleWasCliked =  this.titleWasClicked.bind(this);
-	// }
 
 	titleWasClicked(event) {
 		event.preventDefault();
-		// alert("clicked"+this.props);
+		const { dataCallback } = this.props;
+		if(dataCallback !== undefined) {
+			dataCallback('hello world');
+		}
 	}
 
 	render() {
 		// below statement is equal to const post = this.props.post
 		const { post } = this.props;
-
 		return (
 			<div>
 				<h1 onClick={this.titleWasClicked.bind(this)}>{post.title}</h1>
--- src/posts/PostList.js
+++ src/posts/PostList.js
@@ -3,13 +3,21 @@ import PostData from "../data/post.json";
 import PostDetail from './PostDetail';
 
 class PostList extends Component {
+	handleDataCallback(txtMsg) {
+		alert(txtMsg);
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>hello there!</h1>
 
 				{PostData.map((post, index) => {
-					return <PostDetail post={post}  key={`post-list-key ${index}`} />;
+					return <PostDetail
+						post={post}
+						key={`post-list-key ${index}`}
+						dataCallback={this.handleDataCallback.bind(this)}
+					/>;
 				})}
 			</div>
 		);
