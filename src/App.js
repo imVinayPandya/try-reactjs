b28910496c3f8f8812d38589344e5b3647959263
Vinay Pandya <005vinay@gmail.com>
2018-07-18 19:12:10 +0530

    react-youtube
    react youtube player api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 // import ConstLetVar from './learn/ConstLetVar';
-import PostList from './posts/PostList';
+// import PostList from './posts/PostList';
+
+import ReactYoutubeExample from './thirdParty/ReactYoutubeExample';
 
 class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <PostList />
-        {/* <ConstLetVar /> */}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                {/* <PostList /> */}
+                {/* <ConstLetVar /> */}
+                <ReactYoutubeExample videoId="-_pgcFQ0l64" />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/thirdParty/ReactYoutubeExample.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+
+// https://www.youtube.com/watch?v=-_pgcFQ0l64
+// https://youtu.be/-_pgcFQ0l64
+// https://www.youtube.com/watch?v=-_pgcFQ0l64&index=1&list=PLEsfXFp6DpzQbwYDx1zgcKJ4tzyWFaESK
+class ReactYoutubeExample extends Component {
+
+  componentWillUnmount() {
+    const {playerObj} = this.state;
+    console.log(playerObj.getCurrentTime());
+  }
+
+  videoOnReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.seekTo(50);
+    console.log(event.target);
+    this.setState({
+      playerObj: event.target
+    });
+  }
+
+  videoOnPlay(event) {
+    console.log(event.target.getCurrentTime());
+  }
+
+  videoStateChange(event) {
+    console.log(event.target.getCurrentTime());
+  }
+
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+
+    const { videoId } = this.props;
+
+    return (
+      <YouTube
+        videoId={videoId}
+        opts={opts}
+        onReady={this.videoOnReady}
+        onPlay={this.videoOnPlay}
+        onStateChange={this.videoStateChange}
+      />
+    );
+  }
+
+
+}
+
+export default ReactYoutubeExample;
