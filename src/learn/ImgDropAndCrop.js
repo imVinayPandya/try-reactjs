6e83af28e4fa27cfb7c4bb74f57ce93b4b9b1419
Vinay Pandya <005vinay@gmail.com>
2018-07-30 12:47:09 +0530

    image preview after drop zone

--- src/learn/ImgDropAndCrop.js
+++ src/learn/ImgDropAndCrop.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 
 import Dropzone from 'react-dropzone';
 
-const imageMaxSize = 10; // byte
+const imageMaxSize = 10000000; // byte
 class ImgDropAndCrop extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: null
+    };
+  }
   handleOnDrop = (files, rejectedFiles) => {
     console.log(files);
     console.log(rejectedFiles);
 
+    if (!!files && files.length > 0) {
+      const currentfile = files[0];
+      // for easy use
+      // this.setState({
+      //   imgSrc: currentfile.preview
+      // });
+
+      // convert image to base64 image
+      const myFileReader = new FileReader();
+      myFileReader.addEventListener("load", () => {
+        console.log(myFileReader.result);
+        this.setState({
+          imgSrc: myFileReader.result
+        });
+      }, false);
+
+      myFileReader.readAsDataURL(currentfile);
+    }
+
     if (!!rejectedFiles && rejectedFiles.length > 0) {
       const currentRejectedFile = rejectedFiles[0];
-      const currentRejectedFileType = currentRejectedFile.type;
+      // const currentRejectedFileType = currentRejectedFile.type;
       const currentRejectedFileSize = currentRejectedFile.size;
 
       if (currentRejectedFileSize > imageMaxSize) {
@@ -21,10 +45,22 @@ class ImgDropAndCrop extends Component {
   }
 
   render() {
+    const { imgSrc } = this.state;
     return (
       <div>
         <h1>Drop and Crop</h1>
-        <Dropzone onDrop={this.handleOnDrop} accept='image/*' multiple={false} maxSize={imageMaxSize}>Drop file here</Dropzone>
+
+        {imgSrc !== null ?
+
+          <div>
+            {imgSrc}
+            <img src={imgSrc} alt="you have selected this pic" />
+          </div>
+          :
+          <Dropzone onDrop={this.handleOnDrop} accept='image/*' multiple={false} maxSize={imageMaxSize}>Drop image here or Click to upload image</Dropzone>
+
+        }
+
       </div>
     );
   }
